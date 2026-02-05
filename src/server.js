15b52b89e9ab5300f4e8b0f4604f6152bd422d83
Julia Delis <juliadelis2@hotmail.com>
2026-02-05 16:02:58 -0300

    feat: cors add

--- src/server.js
+++ src/server.js
@@ -16,7 +16,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5174", "http://localhost:5173"],
+    origin: [
+      "http://localhost:5174",
+      "http://localhost:5173",
+      "https://libry-beta.vercel.app/",
+    ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
