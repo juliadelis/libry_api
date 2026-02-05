c2277b232091d69be6af90c0559b10e0e33841d3
Julia Delis <juliadelis2@hotmail.com>
2026-02-04 22:35:37 -0300

    feat: cors police

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
 import { connectDB, disconnectDB } from "./config/db.js";
+import cors from "cors";
 
 //Import Routes
 import bookRoutes from "./routes/bookRoute.js";
@@ -11,6 +12,15 @@ config();
 connectDB();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5174", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
 
 // Body parsing middlewares
 app.use(express.json());
